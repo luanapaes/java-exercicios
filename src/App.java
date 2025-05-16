c9b90e59bb04c0f4037aced196371c8b836f9b7f
Luana Paes <luanafranca598@gmail.com>
2025-05-16 12:31:40 -0300

    instância objetos da classe celular e os imprime

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Celular celularA = new Celular();
+
+        celularA.nome = "Iphone 12";
+        celularA.sistemaOperacional = "iOS";
+        celularA.espacoArmazenamento = 128;
+        celularA.tamanhoTela = 6.1f;
+
+        Celular celularB = new Celular();
+
+        celularB.nome = "Galaxy A51";
+        celularB.sistemaOperacional = "Android";
+        celularB.espacoArmazenamento = 128;
+        celularB.tamanhoTela = 6.5f;
+
+        System.out.println("------------------------------------------------");
+        System.out.format("Celular %s com tela de %.1f, com %dgb de memória e SO %s \n", celularA.nome, celularA.tamanhoTela, celularA.espacoArmazenamento, celularA.sistemaOperacional);
+        System.out.format("Celular %s com tela de %.1f, com %dgb de memória e SO %s", celularB.nome, celularB.tamanhoTela, celularB.espacoArmazenamento, celularB.sistemaOperacional);
     }
 }
