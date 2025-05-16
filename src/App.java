6c2abf947798fcd366db99db1e3877c4755e53fb
Luana Paes <luanafranca598@gmail.com>
2025-05-16 14:27:57 -0300

    adicionando instância de um objeto de um package

--- src/App.java
+++ src/App.java
@@ -1,22 +1,19 @@
+import videos.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         // ---- aula 1 ----------
         // Celular celularA = new Celular();
-
         // celularA.nome = "Iphone 12";
         // celularA.sistemaOperacional = "iOS";
         // celularA.espacoArmazenamento = 128;
         // celularA.tamanhoTela = 6.1f;
-
         // Celular celularB = new Celular();
-
         // celularB.nome = "Galaxy A51";
         // celularB.sistemaOperacional = "Android";
         // celularB.espacoArmazenamento = 128;
         // celularB.tamanhoTela = 6.5f;
-
         // System.out.println("------------------------------------------------");
-
         // System.out.format("Celular %s com tela de %.1f, com %dgb de memória e SO %s \n", celularA.nome, celularA.tamanhoTela, celularA.espacoArmazenamento, celularA.sistemaOperacional);
         // System.out.format("Celular %s com tela de %.1f, com %dgb de memória e SO %s", celularB.nome, celularB.tamanhoTela, celularB.espacoArmazenamento, celularB.sistemaOperacional);
 
@@ -30,7 +27,7 @@ public class App {
         // heroi.verificarHabilidades();
         // heroi.atacar("Goku", "Super forca");
 
-        
+        Filme filme = new Filme();
 
     }
 }
