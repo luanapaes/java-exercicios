ac36126e42d83b3f1d4615e2a93d3754eca05b9c
Luana Paes <luanafranca598@gmail.com>
2025-05-18 15:46:02 -0300

    adiciona classes Circulo e Quadrado implementando a interface FiguraGeometrica

---
+++ src/classes/Quadrado.java
@@ -0,0 +1,21 @@
+package classes;
+
+public class Quadrado implements FiguraGeometrica{
+    private int lado;
+
+    public Quadrado(int lado){
+        this.lado = lado;
+    }
+
+    public double calcularArea(){
+        return Math.pow(lado, 2);
+    }
+
+    public int getLado() {
+        return lado;
+    }
+
+    public void setLado(int lado) {
+        this.lado = lado;
+    }
+}
