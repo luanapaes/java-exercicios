d88c28c694db17263247597cf9abad3ef5b0b35c
Luana Paes <luanafranca598@gmail.com>
2025-05-24 19:50:19 -0300

    adiciona a classe Ave estendendo Animal com método voar

---
+++ src/animais/Ave.java
@@ -0,0 +1,13 @@
+package animais;
+
+public class Ave extends Animal{
+
+    public Ave(String nome) {
+        super(nome);
+    }
+
+    public void voar(){
+        System.out.printf("%s voou.\n", getNome());
+    }
+    
+}
