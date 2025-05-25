49ca7752482e5f58f1e751558a041cdf2caa426f
Luana Paes <luanafranca598@gmail.com>
2025-05-24 23:43:29 -0300

    remove classes and logic related to animal management from the application

--- src/App.java
+++ src/App.java
@@ -1,22 +1,6 @@
-import animais.*;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        Cachorro dog = new Cachorro("Espiga", 3, "Vira-Lata");
-        dog.emitirSom();
-        Gato cat = new Gato("Miguel", 1, "Amarelo");
-        cat.emitirSom();
-        Passaro fly = new Passaro("Bem-Ti-Vi", 4, false);
-        System.out.print(fly);
 
-        Zoologico zoo = new Zoologico();
-        zoo.adicionarAnimal(cat);
-        zoo.adicionarAnimal(dog);
-        zoo.adicionarAnimal(fly);
-        zoo.listarAnimais();
-        zoo.emitirSons();
-
-        zoo.buscarAnimalPorNome("Oliva");
     }
 }
  
